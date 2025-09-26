6dbc68b537d45ce9e0fcf8248ee15cda98ad1ef6
kenenbek <kenenbek@gmail.com>
2025-09-26 10:23:19 +0500

    lora

--- train_latest.py
+++ train_latest.py
@@ -108,6 +108,12 @@ def run(rank, n_gpus, hps):
     _, _, _, epoch_str = utils.load_checkpoint(utils.latest_checkpoint_path(hps.model_dir, "G_*.pth"), net_g, optim_g)
     _, _, _, epoch_str = utils.load_checkpoint(utils.latest_checkpoint_path(hps.model_dir, "D_*.pth"), net_d, optim_d)
     global_step = (epoch_str - 1) * len(train_loader)
+
+    print("-----------------------------------")
+    print(epoch_str)
+    print(len(train_loader))
+    print(global_step)
+    print("-----------------------------------")
   except:
     epoch_str = 1
     global_step = 0
